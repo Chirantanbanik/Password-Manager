b4500d4aa79b931e5cb349d47ac7db1ef7a7abec
Chirantan Banik <banikchirantan16@gmail.com>
2023-01-18 01:35:48 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+master_pwd = input("What is the master password?")
+
+def view():
+  with open('password.docx', 'r') as f:
+    for line in f.readlines():
+      print(line.rstrip())
+   
+def add():
+  name = input('Account name: ')
+  pwd = input('Password: ')
+  
+  with open('password.docx', 'a') as f:
+    f.write(name + " and " + pwd + "\n")
+    
+    
+while True:
+  mode = input("Would you like to add a new password or view the existing ones (view, add), press q to quit?").lower()
+  if mode == "q":
+      break
+  if mode == "view":
+    view()
+  elif mode == "add":
+    add()
+  else:
+    print("Invalid mode.")
+    continue
